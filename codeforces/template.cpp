37d2ee47d3cf0955e4d566332d1b664521df1502
Aniket kumar <ani1998ket@gmail.com>
2022-02-21 02:59:36 +0530

    Adds template code

---
+++ codeforces/template.cpp
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve(){
+    int N;
+    cin >> N;
+    // vector<int> arr(N);
+    // for( auto& i : arr ){
+    //     cin >> i;
+    // }
+}
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
